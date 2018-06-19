2a5ab4e91cb164e0c3aa9f6e6a866a79509f4b05
hexiaozheng <hexiaozhenggood@163.com>
2018-06-19 12:54:59 +0100

    add video playing and recording function in videoReceive.py

--- src/main/python/app/__init__.py
+++ src/main/python/app/__init__.py
@@ -1,7 +1,8 @@
-
+import videoReceive
 
 if __name__ == '__main__':
     print "12321313"
+    videoReceive.videorecording()
 else:
     print "please run __init__.py"
 
--- src/main/python/app/videoReceive.py
+++ src/main/python/app/videoReceive.py
@@ -1,4 +1,36 @@
 import cv2
 import numpy
 
-cap = cv2.VideoCapture()
+
+def videoplaying():
+    cap = cv2.VideoCapture("../../../test/data/video.mov")
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            cv2.imshow("Frame", frame)
+            if cv2.waitKey(100) & 0xFF == ord('q'):
+                break
+        else:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+def videorecording():
+    cap = cv2.VideoCapture(0)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter("../../../../../../123.avi", fourcc, fps, (int(w), int(h)))
+    while 1:
+        ret, frame = cap.read()
+        out.write(frame)
+        cv2.imshow("Frame", frame)
+        if cv2.waitKey(1) & 0xFF ==ord('q'):
+            break
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+
