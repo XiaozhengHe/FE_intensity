1d22e32e65a972d40c5347b5dfe538821ec51dd2
hexiaozheng <hexiaozhenggood@163.com>
2018-07-03 00:09:21 +0100

    use plt imshow function to show lbp image

--- src/main/python/app/localbinarypatterns.py
+++ src/main/python/app/localbinarypatterns.py
@@ -10,14 +10,12 @@ class LocalBinaryPatterns:
         self.numPoints = numpoints
         self.radius = radius
 
-    def describe(self, image, eps=1e-7):
+    def describe(self, gray_image, eps=1e-7):
         # compute the Local Binary Pattern representation
         # of the image, and then use the LBP representation
         # to build the histogram of patterns
-        lbp = feature.local_binary_pattern(image, self.numPoints,
-                                           self.radius, method="uniform")
-        cv2.imshow("lbp", lbp)
-        cv2.waitKey(0)
+        lbp = feature.local_binary_pattern(gray_image, self.numPoints, self.radius, method="default")
+        # cv2.imshow("LBP", lbp.astype(np.uint8)) # show method="default" lbp image, but cannot show method="uniform"
         (hist, _) = np.histogram(lbp.ravel(),
                                  bins=np.arange(0, self.numPoints + 3),
                                  range=(0, self.numPoints + 2))
@@ -30,6 +28,6 @@ class LocalBinaryPatterns:
 
         # return the histogram of Local Binary Patterns
         print "hist after normalizing", hist
-        plt.imshow(lbp)
+        plt.imshow(lbp, cmap="gray")
         plt.show()
         return hist
