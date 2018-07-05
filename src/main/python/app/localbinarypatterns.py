98d50cf97869717a042c668fc030f1b00550e178
hexiaozheng <hexiaozhenggood@163.com>
2018-07-05 19:20:01 +0100

    sub-region in lbp

--- src/main/python/app/localbinarypatterns.py
+++ src/main/python/app/localbinarypatterns.py
@@ -1,6 +1,7 @@
 from skimage import feature
 import numpy as np
 from matplotlib import pyplot as plt
+import cv2
 
 
 class LocalBinaryPatterns:
@@ -9,28 +10,37 @@ class LocalBinaryPatterns:
         self.numPoints = numpoints
         self.radius = radius
 
-    def describe(self, gray_image, eps=1e-7):
+    def describe(self, gray_image, number_subregion, eps=1e-7):
         # compute the Local Binary Pattern representation
         # of the image, and then use the LBP representation
         # to build the histogram of patterns
-        lbp = feature.local_binary_pattern(gray_image, self.numPoints, self.radius, method="uniform")
-        # cv2.imshow("LBP", lbp.astype(np.uint8)) # show method="default" lbp image, but cannot show method="uniform"
-        (hist, _) = np.histogram(lbp.ravel(),
-                                 bins=np.arange(0, self.numPoints + 3),
-                                 range=(0, self.numPoints + 2))
+        height, width = gray_image.shape[:2]
+        region_w = width/number_subregion
+        # print "h+w:", height, width
+        hist = np.array([])
+        if height == width:
+            for i in range(number_subregion**2):
+                row = i // number_subregion
+                col = i % number_subregion
+                region_img = gray_image[region_w*row: region_w*(row+1), region_w*col: region_w*(col+1)]
+                region_lbp = feature.local_binary_pattern(region_img, self.numPoints, self.radius, method="default")
+                cv2.imshow("LBP", region_lbp.astype(np.uint8)) # show method="default" lbp image, but cannot show method="uniform"
+                cv2.waitKey(0)
+                (region_hist, _) = np.histogram(region_lbp.ravel(),
+                                                bins=np.arange(0, self.numPoints + 3),
+                                                range=(0, self.numPoints+2))
 
-        print "hist:", hist
+                # normalize the region histogram
+                region_hist = region_hist.astype("float")
+                region_hist /= (region_hist.sum() + eps)
+                print "single region histogram:", region_hist
 
-        # normalize the histogram
-        hist = hist.astype("float")
-        hist /= (hist.sum() + eps)
 
-        # return the histogram of Local Binary Patterns
-        print "hist after normalizing", hist
-        # showing the lbp image using plt
-        plt.imshow(lbp, cmap="gray")
-        plt.show()
-        print "+++++:", hist+hist
-        t = [hist] + [hist]
-        print "+++++[]:", t
-        return hist
+                # concatenate the region_hist to his
+                hist = np.concatenate((hist, region_hist), axis=0)
+
+            hist = np.reshape(hist, (-1, self.numPoints + 2))
+            print "histogram of all sub-region:", hist
+            return hist
+        else:
+            pass
